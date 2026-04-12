4d71fb06673737470c2857a06a60c9a6d8086777
amirkhan80 <20232417075@siwscollege.edu.in>
2026-04-12 06:14:35 -0700

    Delete core/appointments directory

--- core/appointments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Appointment
-
-@admin.register(Appointment)
-class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ['user', 'service', 'date', 'time', 'status', 'description']
-    list_editable = ['status']
